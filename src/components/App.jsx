ed749a3adfc9cdbfa687d50bc6cb31d3947273fe
basiacarvalho <basiacarvalho@gmail.com>
2023-07-19 22:15:15 +0200

    refactoring of the App.jsx component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,51 +1,41 @@
-import React, { Component } from 'react';
 import { SearchBar } from './searchbar/SearchBar';
 import css from './App.module.css';
 import { ImageGallery } from './imagegallery/ImageGallery';
 import { Modal } from 'components/modal/Modal';
+import { useState } from 'react';
 
-export class App extends Component {
-  state = {
-    searchQuery: '',
-    pageNumber: 1,
-    largePicture: null,
-  };
+export const App = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [pageNumber, setPageNumber] = useState(1);
+  const [largePicture, setLargePicture] = useState(null);
 
-  updateSearchQuery = newSearchQuery => {
-    if (this.state.searchQuery !== newSearchQuery) {
-      this.setState({ searchQuery: newSearchQuery, pageNumber: 1 });
+  const updateSearchQuery = newSearchQuery => {
+    if (searchQuery !== newSearchQuery) {
+      setSearchQuery(newSearchQuery);
+      setPageNumber(1);
     }
   };
 
-  showNextPage = () => {
-    this.setState(prevState => ({ pageNumber: prevState.pageNumber + 1 }));
+  const showNextPage = () => {
+    setPageNumber(prevPageNumber => prevPageNumber + 1);
   };
 
-  showLargePicture = picture => {
-    this.setState({ largePicture: picture });
+  const hideLargePicture = () => {
+    setLargePicture(null);
   };
 
-  hideLargePicture = () => {
-    this.setState({ largePicture: null });
-  };
-
-  render() {
-    return (
-      <div className={css.app}>
-        <SearchBar handleSubmit={this.updateSearchQuery} />
-        <ImageGallery
-          searchQuery={this.state.searchQuery}
-          currentPage={this.state.pageNumber}
-          showNextPage={this.showNextPage}
-          showLargePicture={this.showLargePicture}
-        />
-        {this.state.largePicture !== null && (
-          <Modal
-            picture={this.state.largePicture}
-            hideModal={this.hideLargePicture}
-          />
-        )}
-      </div>
-    );
-  }
-}
+  return (
+    <div className={css.app}>
+      <SearchBar handleSubmit={updateSearchQuery} />
+      <ImageGallery
+        searchQuery={searchQuery}
+        currentPage={pageNumber}
+        showNextPage={showNextPage}
+        showLargePicture={setLargePicture}
+      />
+      {largePicture !== null && (
+        <Modal picture={largePicture} hideModal={hideLargePicture} />
+      )}
+    </div>
+  );
+};
