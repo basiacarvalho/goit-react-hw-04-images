da9e248852671727f07f77442e4b7514915fb621
basiacarvalho <basiacarvalho@gmail.com>
2023-07-19 21:29:27 +0200

    refactoring of the imagegalleryitem

--- src/components/imagegalleryitem/ImageGalleryItem.jsx
+++ src/components/imagegalleryitem/ImageGalleryItem.jsx
@@ -1,17 +1,14 @@
-import React, { Component } from 'react';
 import css from './ImageGalleryItem.module.css';
 
-export class ImageGalleryItem extends Component {
-  render() {
-    return (
-      <li className={css.galleryItem}>
-        <img
-          className={css.image}
-          src={this.props.src}
-          alt={this.props.alt}
-          onClick={() => this.props.onClick(this.props.largeImage)}
-        />
-      </li>
-    );
-  }
-}
+export const ImageGalleryItem = ({ src, alt, onClick, largeImage }) => {
+  return (
+    <li className={css.galleryItem}>
+      <img
+        className={css.image}
+        src={src}
+        alt={alt}
+        onClick={() => onClick(largeImage)}
+      />
+    </li>
+  );
+};
