34b472ab7b2ba3bf71a6e500fc6b903f2b63621f
basiacarvalho <basiacarvalho@gmail.com>
2023-07-19 18:49:09 +0200

    refactoring the button component

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,16 +1,9 @@
-import React, { Component } from 'react';
 import css from './Button.module.css';
 
-export class Button extends Component {
-  render() {
-    return (
-      <button
-        type="button"
-        className={css.button}
-        onClick={this.props.showNextPage}
-      >
-        Load more
-      </button>
-    );
-  }
-}
+export const Button = () => {
+  return (
+    <button type="button" className={css.button} onClick={showNextPage}>
+      Load more
+    </button>
+  );
+};
