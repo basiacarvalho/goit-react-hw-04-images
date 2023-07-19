e0c2b5e8ef696f8af58b836b9cc440170a20ec6a
basiacarvalho <basiacarvalho@gmail.com>
2023-07-19 21:47:31 +0200

    refactoring of the searchbar component

--- src/components/searchbar/SearchBar.jsx
+++ src/components/searchbar/SearchBar.jsx
@@ -1,29 +1,26 @@
-import React, { Component } from 'react';
 import css from './SearchBar.module.css';
 
-export class SearchBar extends Component {
-  searchImages = evt => {
+export const SearchBar = ({ handleSubmit }) => {
+  const searchImages = evt => {
     evt.preventDefault();
-    this.props.handleSubmit(evt.target.searchQuery.value);
+    handleSubmit(evt.target.searchQuery.value);
   };
 
-  render() {
-    return (
-      <header className={css.searchBar}>
-        <form className={css.form} onSubmit={this.searchImages}>
-          <button type="submit" className={css.button}>
-            <span className={css.buttonLabel}>Search</span>
-          </button>
-          <input
-            className={css.input}
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-            name="searchQuery"
-          />
-        </form>
-      </header>
-    );
-  }
-}
+  return (
+    <header className={css.searchBar}>
+      <form className={css.form} onSubmit={searchImages}>
+        <button type="submit" className={css.button}>
+          <span className={css.buttonLabel}>Search</span>
+        </button>
+        <input
+          className={css.input}
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          name="searchQuery"
+        />
+      </form>
+    </header>
+  );
+};
