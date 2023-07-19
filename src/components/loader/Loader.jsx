4bb5b581fbf7da3cd18e9165f2a96564c65f374a
basiacarvalho <basiacarvalho@gmail.com>
2023-07-19 21:31:19 +0200

    refactoring of the loader component

--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -1,23 +1,20 @@
-import { Component } from 'react';
 import { Circles } from 'react-loader-spinner';
 import css from './Loader.module.css';
 
-export class Loader extends Component {
-  render() {
-    return (
-      <div className={css.backdrop}>
-        <div className={css.loader}>
-          <Circles
-            height="80"
-            width="80"
-            color="#4fa94d"
-            ariaLabel="circles-loading"
-            wrapperStyle={{}}
-            wrapperClass=""
-            visible={true}
-          />
-        </div>
+export const Loader = () => {
+  return (
+    <div className={css.backdrop}>
+      <div className={css.loader}>
+        <Circles
+          height="80"
+          width="80"
+          color="#4fa94d"
+          ariaLabel="circles-loading"
+          wrapperStyle={{}}
+          wrapperClass=""
+          visible={true}
+        />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
